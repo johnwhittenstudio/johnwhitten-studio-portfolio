82988d59e2b36aff242ff82f039db9a81915d75c
John Whitten <johnwhitten.studio@gmail.com>
2022-10-21 11:03:16 -0700

    update about to fix typos

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,7 @@ const About = () => {
       <div className={classes.Container}>
         <div className={classes.Text}>
             <p>
-            Hello, My name is John Whitten. I am a JavaScript, React.js, and WordPress developer. I particulary enjoy front end work. As a full stack developer, I am comfortable with working at all levels of the development process; from wireframing and implementing a design system, to integrating a back end and database. Beautiful and responsive UI, as well as clean, maintainable, and accessible code are important to me. 
+            Hello, My name is John Whitten. I am a JavaScript, React.js, and WordPress developer. I particularly enjoy front end work. As a full stack developer, I am comfortable with working at all levels of the development process; from wire-framing and implementing a design system to integrating a back end and database. Beautiful and responsive UI, as well as clean, maintainable, and accessible code are important to me. 
             <br></br>
             I thrive in a team that values collaborative problem solving, sophisticated design, and quality work. I am a veresatile developer who adapts to new challanges, technologies, and methodolgies. I am comfortable using a variety of technologies such as Typescript, ReactJS, NodeJs, MaterialUI, PHP, and numerous others. Please visit my <a href="https://github.com/johnwhittenstudio">GitHub</a>, give me a star, explore, and enjoy...
             </p>
